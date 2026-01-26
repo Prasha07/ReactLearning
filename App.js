c010b452e0a8494d1309de71475775167b7eea91
Prasha07 <prashantshukla0710@gmail.com>
2026-01-26 15:58:15 +0530

    Igniting our app - Created a new mini create react app

--- App.js
+++ App.js
@@ -1,5 +1,15 @@
-const h1 = React.createElement("h1", { id: "title" }, "Heading 1");
-const h2 = React.createElement("h2", { id: "title" }, "Heading 2");
-const div = React.createElement("h1", { id: "container" }, [h1, h2]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const h1 = React.createElement(
+  "h1",
+  { key: "h1", id: "title1", hello: "world" },
+  "Heading 1",
+);
+const h2 = React.createElement("h2", { key: "h2", id: "title2" }, "Heading 2");
+const div = React.createElement("div", { key: "di", id: "container" }, [
+  h1,
+  h2,
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(div);
