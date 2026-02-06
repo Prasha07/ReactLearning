f6ca5fccb1513d99d343c158b1c7e3d01a4b1b4b
Prasha07 <prashantshukla0710@gmail.com>
2026-02-06 08:18:16 +0530

    Lec-4 laying the foundtion is completed.

--- App.js
+++ App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const h1 = React.createElement(
-  "h1",
-  { key: "h1", id: "title1", hello: "world" },
-  "Heading 1",
-);
-const h2 = React.createElement("h2", { key: "h2", id: "title2" }, "Heading 2");
-const div = React.createElement("div", { key: "di", id: "container" }, [
-  h1,
-  h2,
-]);
+// const h1 = React.createElement(
+//   "h1",
+//   { key: "h1", id: "title1", hello: "world" },
+//   "Heading 1",
+// );
+// const h2 = React.createElement("h2", { key: "h2", id: "title2" }, "Heading 2");
+// const div = React.createElement("div", { id: "container" }, [h1, h2]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(div);
+
+const Title = () => <h1 className="class">Hello bro</h1>;
+var xyz = 10;
+const HeaderComponent = () => {
+  return (
+    <div>
+      {console.log(xyz)}
+      {/* <Title /> */}
+      <h1>this is h1</h1>
+      <h2>this is h2</h2>
+    </div>
+  );
+};
+root.render(<HeaderComponent />);
